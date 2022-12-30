cf2e580cec28f57da5f6db586d7bf81f14caf15e
Mandeep <50768465+mandeepsinghkhaira@users.noreply.github.com>
2022-12-30 17:18:20 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+//selectors
+let secondsSpan = document.querySelector("#seconds");
+let minutesSpan = document.querySelector("#minutes");
+let hoursSpan = document.querySelector("#hours");
+let daysSpan = document.querySelector("#days");
+
+function countDown(){
+let currentDate = new Date();
+let endOfYear = new Date(new Date().getFullYear(), 11, 31);
+let difference = (endOfYear - currentDate) / 1000; // seconds from milliseconds
+let secondsLeft = Math.floor(difference) % 60;
+let minutesLeft = Math.floor(difference / 60) % 60;
+let hoursLeft = Math.floor(difference / 60 / 60) % 24; // % accounts for the number of hours passed already
+let daysLeft = endOfYear.getDay() - currentDate.getDay();
+secondsSpan.innerText = secondsLeft < 10 ? ` 0${secondsLeft}`: ` ${secondsLeft}`;
+minutesSpan.innerText = minutesLeft < 10 ? ` 0${minutesLeft}`: ` ${minutesLeft}`;
+hoursSpan.innerText = hoursLeft < 10 ? ` 0${hoursLeft}`: ` ${hoursLeft}`;
+daysSpan.innerText = daysLeft < 10 ? ` 0${daysLeft}`: ` ${daysLeft}`;
+};
+
+
+
+setInterval(countDown, 1000)
+
