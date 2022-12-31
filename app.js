f315b07af7de06ed20b99162c184a98ab025e770
Mandeep <50768465+mandeepsinghkhaira@users.noreply.github.com>
2022-12-31 01:43:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ let daysSpan = document.querySelector("#days");
 
 function countDown(){
 let currentDate = new Date();
-let endOfYear = new Date(new Date().getFullYear(), 11, 31);
+let endOfYear = new Date(`01 January ${currentDate.getFullYear()+1} 00:00:00`);
 let difference = (endOfYear - currentDate) / 1000; // seconds from milliseconds
 let secondsLeft = Math.floor(difference) % 60;
 let minutesLeft = Math.floor(difference / 60) % 60;
 let hoursLeft = Math.floor(difference / 60 / 60) % 24; // % accounts for the number of hours passed already
-let daysLeft = endOfYear.getDay() - currentDate.getDay();
+let daysLeft = Math.floor(difference / 60 / 60 / 24);
 secondsSpan.innerText = secondsLeft < 10 ? ` 0${secondsLeft}`: ` ${secondsLeft}`;
 minutesSpan.innerText = minutesLeft < 10 ? ` 0${minutesLeft}`: ` ${minutesLeft}`;
 hoursSpan.innerText = hoursLeft < 10 ? ` 0${hoursLeft}`: ` ${hoursLeft}`;
 daysSpan.innerText = daysLeft < 10 ? ` 0${daysLeft}`: ` ${daysLeft}`;
+
 };
 
 
 
 setInterval(countDown, 1000)
-
